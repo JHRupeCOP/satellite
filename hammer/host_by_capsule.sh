249eed2c6855b993059c102453d0461e5be30644
James Ruppert <james.ruppert@conocophillips.net>
2024-10-01 07:47:14 -0500

    updated host_by_capsule.sh; removed grep and added hammer fields

--- hammer/host_by_capsule.sh
+++ hammer/host_by_capsule.sh
@@ -2,10 +2,10 @@
 
 export REPORT_DIR="/home/ruppej/reports"
   
-for CAPSULE in $(/bin/hammer --no-headers  proxy list | cut -d"|" -f2)
+for CAPSULE in $(/bin/hammer --no-headers  proxy list --fields name)
 do
   export CAPSULE_SHORT=$(echo $CAPSULE | cut -d. -f1)
 
-  /bin/hammer --csv --output-file "${REPORT_DIR}/hostBycapsule_${CAPSULE_SHORT}.csv" \
-   host list --search="registered_through = ${CAPSULE}"
+  /bin/hammer --no-headers --csv --output-file "${REPORT_DIR}/hostBycapsule_${CAPSULE_SHORT}.csv" \
+   host list --fields "name,operating system,host group,ip,lifecycle environment" --search="registered_through = ${CAPSULE}"
 done
