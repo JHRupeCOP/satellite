15f11cb5199dd72569fc19c390ee2c9ef05a1c14
James Ruppert <james.ruppert@conocophillips.net>
2024-10-17 15:25:48 -0500

    minor update to hammer/host_by_hostgroup.sh

--- hammer/host_by_hostgroup.sh
+++ hammer/host_by_hostgroup.sh
@@ -20,6 +20,7 @@ fi
 
 for host_group in $(/bin/hammer --no-headers hostgroup list --fields name)
 do
+  echo -e "Generating report for host group: ${host_group}"
   /bin/hammer --no-headers --csv --output-file "${REPORT_DIR}/host_by_hostgroup_${host_group}.csv" \
      host list --search "hostgroup = $host_group" --fields "name"
 done
